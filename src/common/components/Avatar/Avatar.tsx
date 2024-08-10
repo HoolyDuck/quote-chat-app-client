48d017287c696821bc428ed20d974d54ae8b432c
HoolyDuck <nazar260804@gmail.com>
2024-08-11 02:25:05 +0300

    + default avatar img

--- src/common/components/Avatar/Avatar.tsx
+++ src/common/components/Avatar/Avatar.tsx
@@ -1,15 +1,16 @@
 import styles from "./styles.module.css";
+import noAvatar from "@/assets/images/no-avatar.png";
 
 type AvatarProps = {
-  src: string;
-  alt: string;
+  src?: string;
+  alt?: string;
 };
 
 export const Avatar = ({ src, alt }: AvatarProps) => {
   return (
     <img
-      src={src}
-      alt={alt}
+      src={src || noAvatar}
+      alt={alt || "avatar"}
       className={styles.avatar}
     />
   );
