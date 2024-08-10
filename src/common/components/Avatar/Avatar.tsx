3dcb7cfb39fe3e531b2181878fe88a9a5df23890
HoolyDuck <nazar260804@gmail.com>
2024-08-11 01:47:30 +0300

    * avatar to separate component

---
+++ src/common/components/Avatar/Avatar.tsx
@@ -0,0 +1,16 @@
+import styles from "./styles.module.css";
+
+type AvatarProps = {
+  src: string;
+  alt: string;
+};
+
+export const Avatar = ({ src, alt }: AvatarProps) => {
+  return (
+    <img
+      src={src}
+      alt={alt}
+      className={styles.avatar}
+    />
+  );
+};
--- src/pages/ChatPage/components/SidebarHeader/SidebarHeader.tsx
+++ src/pages/ChatPage/components/SidebarHeader/SidebarHeader.tsx
@@ -2,15 +2,15 @@ import { Input } from "@/common/components/Input/Input";
 import styles from "./styles.module.css";
 import { Button } from "@/common/components/Button/Button";
 import { SearchIcon } from "@/assets/icons/SearchIcon";
+import { Avatar } from "@/common/components/Avatar/Avatar";
 
 export const SidebarHeader = () => {
   return (
     <div className={styles.side_header}>
       <div className={styles.avatar_wrapper}>
-        <img
-          src="https://via.placeholder.com/150"
+        <Avatar
+          src="https://picsum.photos/200"
           alt="avatar"
-          className={styles.avatar}
         />
         <Button size="small">Login</Button>
       </div>
