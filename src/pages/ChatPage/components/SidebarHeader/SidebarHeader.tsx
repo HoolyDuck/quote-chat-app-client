477338394a91df48ae0897c17293d8e9108c59c4
HoolyDuck <nazar260804@gmail.com>
2024-08-11 02:28:37 +0300

    * user avatar and login/logout button

--- src/pages/ChatPage/components/SidebarHeader/SidebarHeader.tsx
+++ src/pages/ChatPage/components/SidebarHeader/SidebarHeader.tsx
@@ -3,21 +3,35 @@ import styles from "./styles.module.css";
 import { Button } from "@/common/components/Button/Button";
 import { SearchIcon } from "@/assets/icons/SearchIcon";
 import { Avatar } from "@/common/components/Avatar/Avatar";
+import { useAppSelector } from "@/lib/store/hooks";
 
 export const SidebarHeader = () => {
+  const { user } = useAppSelector((state) => state.user);
+
+  const logInOrOutButton = user ? (
+    <Button
+      size="small"
+      variant="secondary"
+    >
+      Log out
+    </Button>
+  ) : (
+    <Button
+      size="small"
+      variant="primary"
+    >
+      Log in
+    </Button>
+  );
+
   return (
     <div className={styles.side_header}>
       <div className={styles.avatar_wrapper}>
         <Avatar
-          src="https://picsum.photos/200"
-          alt="avatar"
+          src={user?.avatar}
+          alt="User avatar"
         />
-        <Button
-          size="small"
-          variant="secondary"
-        >
-          Log in
-        </Button>
+        {logInOrOutButton}
       </div>
       <Input
         placeholder="Search for chats"
