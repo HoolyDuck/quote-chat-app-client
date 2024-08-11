d6a1914525220f581fd32c2dcf81355e91e78450
HoolyDuck <nazar260804@gmail.com>
2024-08-11 03:03:31 +0300

    + chat fetching

--- src/pages/ChatPage/components/ChatList/ChatList.tsx
+++ src/pages/ChatPage/components/ChatList/ChatList.tsx
@@ -1,17 +1,27 @@
+import { useGetChatsQuery } from "@/lib/api/chat/chatApi";
 import { ChatCard } from "../ChatCard/ChatCard";
 import styles from "./styles.module.css";
 
 export const ChatList = () => {
+  const { data, isLoading } = useGetChatsQuery();
+  const chats = data?.chats;
+
   return (
     <div className={styles.chat_list}>
       <h2 className={styles.title}>Chats</h2>
       <div className={styles.chats_container}>
-        <ChatCard
-          avatarSrc="https://picsum.photos/200"
-          name="John Doe"
-          lastMessage="Hello, how are yofassssssssssssssssssssssssssssssssssssu?"
-          lastMessageTime="10:00"
-        />
+        {isLoading ? (
+          <p>Loading...</p>
+        ) : (
+          chats?.map((chat) => (
+            <ChatCard
+              key={chat._id}
+              name={`${chat.firstName} ${chat.lastName}`}
+              lastMessage={chat.messages[0]}
+              lastMessageTime={new Date().toLocaleTimeString()}
+            />
+          ))
+        )}
       </div>
     </div>
   );
