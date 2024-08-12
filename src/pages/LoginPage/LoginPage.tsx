48e2a607fbc03f625b6b6207935319c1ea622325
HoolyDuck <nazar260804@gmail.com>
2024-08-12 05:24:02 +0300

    * cookie test

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,20 +1,29 @@
 import { Button } from "@/common/components/Button/Button";
 import styles from "./styles.module.css";
 import { Container } from "@/common/components/Container/Container";
-
+import { useEffect } from "react";
 const googleAuthUrl = `${import.meta.env.VITE_API_URL}/auth/google`;
 
 export const LoginPage = () => {
+  useEffect(() => {
+    fetch(`${import.meta.env.VITE_API_URL}/auth/test`, {
+      credentials: "include",
+    });
+    console.log("test");
+  }, []);
+
   return (
     <Container>
       <div className={styles.login_box}>
         <h1 className={styles.title}>Welcome!</h1>
-        <Button
-          size="large"
-          width="full"
-        >
-          <a href={googleAuthUrl}>Login with Google</a>
-        </Button>
+        <a href={googleAuthUrl}>
+          <Button
+            size="large"
+            width="full"
+          >
+            Login with Google
+          </Button>
+        </a>
       </div>
     </Container>
   );
