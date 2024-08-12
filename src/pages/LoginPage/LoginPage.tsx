809e0d587c03549d78139190670ba4e522ff50e6
HoolyDuck <nazar260804@gmail.com>
2024-08-12 18:53:02 +0300

    * remove debug

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,17 +1,10 @@
 import { Button } from "@/common/components/Button/Button";
 import styles from "./styles.module.css";
 import { Container } from "@/common/components/Container/Container";
-import { useEffect } from "react";
+
 const googleAuthUrl = `${import.meta.env.VITE_API_URL}/auth/google`;
 
 export const LoginPage = () => {
-  useEffect(() => {
-    fetch(`${import.meta.env.VITE_API_URL}/auth/test`, {
-      credentials: "include",
-    });
-    console.log("test");
-  }, []);
-
   return (
     <Container>
       <div className={styles.login_box}>
