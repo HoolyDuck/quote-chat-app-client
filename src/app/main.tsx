071687884502d50dfffd464e9204be2d6aea7960
HoolyDuck <nazar260804@gmail.com>
2024-08-13 17:28:28 +0300

    * prettier select chat

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -8,38 +8,44 @@ import { Provider } from "react-redux";
 import { store } from "@/lib/store/store.ts";
 import { ChatPage } from "@/pages/ChatPage/ChatPage.tsx";
 import { ChatDisplay } from "@/pages/ChatDisplay/ChatDisplay.tsx";
+import { Navigate } from "react-router-dom";
+import { SelectChatDisplay } from "@/pages/ChatDisplay/components/SelectChatDisplay/SelectChatDisplay.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
- // <React.StrictMode>
-    <Provider store={store}>
-      <RouterProvider
-        routes={[
-          {
-            path: "/",
-            element: <App />,
-            children: [
-              {
-                path: "/login",
-                element: <LoginPage />,
-              },
-              {
-                path: "/chat",
-                element: <ChatPage />,
-                children: [
-                  {
-                    path: "/chat",
-                    element: <p>Select chat</p>
-                  },
-                  {
-                    path: "/chat/:chatId",
-                    element: <ChatDisplay />,
-                  },
-                ],
-              },
-            ],
-          },
-        ]}
-      />
-    </Provider>
+  // <React.StrictMode>
+  <Provider store={store}>
+    <RouterProvider
+      routes={[
+        {
+          path: "/",
+          element: <App />,
+          children: [
+            {
+              path: "/login",
+              element: <LoginPage />,
+            },
+            {
+              path: "/chat",
+              element: <ChatPage />,
+              children: [
+                {
+                  path: "/chat",
+                  element: <SelectChatDisplay />,
+                },
+                {
+                  path: "/chat/:chatId",
+                  element: <ChatDisplay />,
+                },
+              ],
+            },
+            {
+              path: "*",
+              element: <Navigate to="/login" />,
+            },
+          ],
+        },
+      ]}
+    />
+  </Provider>
   //</React.StrictMode>
 );
---
+++ src/pages/ChatDisplay/components/SelectChatDisplay/SelectChatDisplay.tsx
@@ -0,0 +1,9 @@
+import styles from "./styles.module.css";
+
+export const SelectChatDisplay = () => {
+  return (
+    <div className={styles.select_chat}>
+      <p>Select or create chat</p>
+    </div>
+  );
+};
