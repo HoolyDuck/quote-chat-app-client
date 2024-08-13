371d65a44fd1b63138840e98b147f9e3b1bf2326
HoolyDuck <nazar260804@gmail.com>
2024-08-13 21:35:07 +0300

    * logging out for real

--- src/lib/api/auth/authApi.ts
+++ src/lib/api/auth/authApi.ts
@@ -6,8 +6,14 @@ const authApi = apiSlice.injectEndpoints({
     getProfile: builder.query<User, void>({
       query: () => "/auth/profile",
     }),
+    logOut: builder.mutation<void, void>({
+      query: () => ({
+        url: "/auth/logout",
+        method: "POST",
+      }),
+    }),
   }),
 });
 
-export const { useGetProfileQuery } = authApi;
+export const { useGetProfileQuery, useLogOutMutation } = authApi;
 export { authApi };
