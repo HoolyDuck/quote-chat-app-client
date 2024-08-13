1beea377dbcae959e7eadf78ceb10ae9d3cf1b46
HoolyDuck <nazar260804@gmail.com>
2024-08-13 20:00:18 +0300

    * replace with env var

--- src/lib/socket/index.ts
+++ src/lib/socket/index.ts
@@ -1,7 +1,7 @@
 import { createContext } from "react";
 import { io } from "socket.io-client";
 
-const socket = io("ws://localhost:3000", {
+const socket = io(import.meta.env.VITE_API_URL as string, {
   autoConnect: false,
   withCredentials: true,
 });
